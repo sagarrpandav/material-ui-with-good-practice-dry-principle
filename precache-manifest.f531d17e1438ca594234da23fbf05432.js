177305b96964f39a7fa052efc09f7b671933adb8
sagarrpandav <sagar.rpandav@gmail.com>
2021-08-28 15:23:19 -0400

    Updates

---
+++ precache-manifest.f531d17e1438ca594234da23fbf05432.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7c341daa182f273f2933263369d653",
+    "url": "/material-ui-with-good-practice-dry-principle/index.html"
+  },
+  {
+    "revision": "a99659c2e1080abee3d8",
+    "url": "/material-ui-with-good-practice-dry-principle/static/css/main.838a69de.chunk.css"
+  },
+  {
+    "revision": "892db7c2cdfd5432dc90",
+    "url": "/material-ui-with-good-practice-dry-principle/static/js/2.83cb9263.chunk.js"
+  },
+  {
+    "revision": "936711c3f4be0c9c88207c43cda44117",
+    "url": "/material-ui-with-good-practice-dry-principle/static/js/2.83cb9263.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a99659c2e1080abee3d8",
+    "url": "/material-ui-with-good-practice-dry-principle/static/js/main.3f46d935.chunk.js"
+  },
+  {
+    "revision": "d82695a1275460bcb672",
+    "url": "/material-ui-with-good-practice-dry-principle/static/js/runtime-main.745a5697.js"
+  }
+]);
