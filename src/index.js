673af3f7022aced340f6707b8941ea961db5f20e
Sagar R Pandav <sagar.rpandav@gmail.com>
2022-05-04 16:18:26 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,3 +13,4 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+window.location.href = "http://hub.itp.io:5000/";
